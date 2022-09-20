ceed6046955e952ac6099726821d3ad89c0b67c0
Shaker Sharabi <sheko100@icloud.com>
2022-09-20 11:00:32 +0200

    feat: Use routing middleware

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import express from "express";
-import request from "supertest";
+import routes from "./routes/index"
 
 const app = express();
 const port = 3000;
 
-app.get("/api", (req, res) => {
-  res.send("api endpoint");
-});
+app.use("/api", routes);
 
 
 app.listen(port, () => {
