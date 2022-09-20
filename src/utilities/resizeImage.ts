f4193eb37e4379e54a624c9fcf141d3ef673b31b
Shaker Sharabi <sheko100@icloud.com>
2022-09-20 15:23:11 +0200

    refactor: remove try and catch block

--- src/utilities/resizeImage.ts
+++ src/utilities/resizeImage.ts
@@ -1,15 +1,14 @@
 import sharp from "sharp";
 
-const resizeImage = async (imageName: string, width: number, height: number, outputImage: string): Promise<void> => {
-
+const resizeImage = async (
+  imageName: string,
+  width: number,
+  height: number,
+  outputImage: string
+): Promise<void> => {
   const inputImage = `src/assets/full/${imageName}.jpg`;
 
-  try {
-    const transform = await sharp(inputImage).resize(width, height).toFile(outputImage);
-  } catch (err) {
-    throw err;
-  }
-
-}
+  await sharp(inputImage).resize(width, height).toFile(outputImage);
+};
 
 export default resizeImage;
