d93316841ca1ba0bce7eb222ef12b3e677e345c9
Shaker Sharabi <sheko100@icloud.com>
2022-09-21 11:56:56 +0200

    test: change the endpoint url

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -5,7 +5,7 @@ const request = supertest(app);
 
 describe("testing the endpoint", (): void => {
   it("gets the api ednpoint", async (): Promise<void> => {
-    const response = await request.get("/api");
+    const response = await request.get("/api/images?file_name=santamonica&width=48&height=48");
     expect(response.status).toBe(200);
   });
 });
