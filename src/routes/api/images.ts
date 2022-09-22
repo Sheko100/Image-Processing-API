8ecfe72a9ed1430bb3717304271810be5357298a
Shaker Sharabi <sheko100@icloud.com>
2022-09-22 15:29:29 +0200

    refactor: Check the resized image dimensions

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,28 +1,41 @@
 import express from "express";
 import resizeImage from "../../utilities/resizeImage";
+import sharp from "sharp";
 import path from "path";
 
 const route = express.Router();
 
-route.get("/", async (req: express.Request, res: express.Response): Promise<void>  => {
-  const imageName = req.query.file_name as string;
-  const width = parseInt(req.query.width as string);
-  const height = parseInt(req.query.height as string);
-  const outputImage = `./assets/thumb/${imageName}.jpg`;
-  const imagePath = path.resolve(outputImage);
-  let errMessage =
-    "Error: there is an error during image processing. Please, refresh the page to try again";
+route.get(
+  "/",
+  async (req: express.Request, res: express.Response): Promise<void> => {
+    const imageName = req.query.file_name as string;
+    const width = parseInt(req.query.width as string);
+    const height = parseInt(req.query.height as string);
+    const outputImage = `./assets/thumb/${imageName}.jpg`;
+    const imagePath = path.resolve(outputImage);
+    let errMessage =
+      "Error: there is an error during image processing. Please, refresh the page to try again";
+    try {
+      const metadata = await sharp(outputImage).metadata();
 
-  try {
-    await resizeImage(imageName, width, height, outputImage);
-    res.sendFile(imagePath);
-  } catch (err) {
-    if (err instanceof Error) {
-      errMessage = err.message;
+      if (metadata) {
+        if (metadata.width == width && metadata.height == height) {
+          res.sendFile(imagePath);
+        }
+      }
+    } catch (err) {
+      try {
+        await resizeImage(imageName, width, height, outputImage);
+        res.sendFile(imagePath);
+      } catch (err) {
+        if (err instanceof Error) {
+          errMessage = err.message;
+        }
+        console.error(err);
+        res.send(errMessage);
+      }
     }
-    console.error(err);
-    res.send(errMessage);
   }
-});
+);
 
 export default route;
